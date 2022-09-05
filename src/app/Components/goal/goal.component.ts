4acb22d6e1872be21eefd13a569dc3b7d94a76a5
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-09-05 18:52:12 +0300

    creating goals array for hardcoded goals

--- src/app/Components/goal/goal.component.ts
+++ src/app/Components/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from 'src/app/Classes/goal';
 
 @Component({
   selector: 'app-goal',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+
+  goals: Goal[] = [
+    
+    new Goal(1, "Angular.", "Master angular for frontend purposes.", new Date(2022, 8, 30)),
+    new Goal(2, "Django.", "Master Django for Backend purposes.", new Date(2022, 9, 31)),
+    new Goal(3, "Projects.", "Do practice on personal projects and real clients projects.", new Date(2022, 11, 30))
+    
+  ]
 
   constructor() { }
 
