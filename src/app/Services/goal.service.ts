0fd3e37c4b6f8f6c9be231aba7d1554a83ec1529
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-09-07 16:31:33 +0300

    creating a function to fetch a single goal from the arry using it's id

--- src/app/Services/goal.service.ts
+++ src/app/Services/goal.service.ts
@@ -6,8 +6,19 @@ import { Goals } from '../Backend/goal-list';
 })
 export class GoalService {
 
+  //Getting all the goals
   getGoals() {
     return Goals;
+  }
+
+  //Fetching a single goal
+  getGoal(id: number) {
+    for (let goal of Goals) {
+      if (goal.id == id) {
+        return goal;
+      }
+    }
+    return
   }
 
   constructor() { }
