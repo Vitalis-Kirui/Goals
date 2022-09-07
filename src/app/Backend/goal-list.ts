c0e8ab041eaaf8bb82edf5bd09cc2778fefeaf7f
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-09-07 14:45:51 +0300

    creating a temporary backend for goal list

---
+++ src/app/Backend/goal-list.ts
@@ -0,0 +1,7 @@
+import { Goal } from "../Classes/goal";
+
+export let Goals: Goal[] = [
+    new Goal(1, "Angular.", "Master angular for frontend purposes.", new Date(2022, 8, 30)),
+    new Goal(2, "Django.", "Master Django for Backend purposes.", new Date(2022, 9, 31)),
+    new Goal(3, "Projects.", "Do practice on personal projects and real clients projects.", new Date(2022, 11, 30))
+];
--- src/app/Components/goal/goal.component.ts
+++ src/app/Components/goal/goal.component.ts
@@ -8,13 +8,15 @@ import { Goal } from 'src/app/Classes/goal';
 })
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
+  goals! : Goal[];
+
+  // goals: Goal[] = [
     
-    new Goal(1, "Angular.", "Master angular for frontend purposes.", new Date(2022, 8, 30)),
-    new Goal(2, "Django.", "Master Django for Backend purposes.", new Date(2022, 9, 31)),
-    new Goal(3, "Projects.", "Do practice on personal projects and real clients projects.", new Date(2022, 11, 30))
+  //   new Goal(1, "Angular.", "Master angular for frontend purposes.", new Date(2022, 8, 30)),
+  //   new Goal(2, "Django.", "Master Django for Backend purposes.", new Date(2022, 9, 31)),
+  //   new Goal(3, "Projects.", "Do practice on personal projects and real clients projects.", new Date(2022, 11, 30))
     
-  ]
+  // ]
 
   //Delete button functionality
   delete(index: number) {
